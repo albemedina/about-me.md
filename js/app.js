f105312d73d23d8722e615d91af3ca064d26ed62
albemedina <medinaa.atc@gmail.com>
2021-04-28 23:00:17 -0700

    updated

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let username = prompt('Hey can I ask for your name ?')
+
+alert('Wanna talk for just a second + username + ?');
+
+ let questionOne = prompt('Do you like Grey/s Anatomy? Type yes or no);
+
+if(questionOne === 'yes' || questionOne === 'y'){
+
+}
