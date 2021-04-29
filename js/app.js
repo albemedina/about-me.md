fca4699a72545e5e10e419d4094e36c3337e4274
albemedina <medinaa.atc@gmail.com>
2021-04-28 23:38:46 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,55 @@
 'use strict'
 
-let username = prompt('Hey can I ask for your name ?')
+let username = prompt('Hey can I ask for your name ?');
 
-alert('Wanna talk for just a second + username + ?');
+alert('Wanna talk for just a second + username + ? Answer yes or no y/n');
 
- let questionOne = prompt('Do you like Grey/s Anatomy? Type yes or no);
+ let questionOne = prompt('Do you like Grey/s Anatomy? Type yes or no');
+  // console.log(questionOne)
+ if(questionOne === 'no'  || questionOne === 'n'){
 
-if(questionOne === 'yes' || questionOne === 'y'){
+ }
+else if(questionOne === 'yes' || questionOne === 'y'){
 
+}
+
+alert('me either I dont watch it')
+
+ let questionTwo = prompt('Do you live in washington state');
+ 
+ // console.log(questionTwo)
+ if(questionTwo === 'no'  || questionOne === 'n'){
+    
+    alert('If only I could leave');
+ }
+
+
+else if(questionTwo === 'yes' || questionTwo === 'y'){
+    
+    alert('Me too!')
+}
+
+let questionThree = prompt('Have you ever been in the service');
+ // console.log(questionThree)
+ if(questionThree === 'no'  || questionThree === 'n'){
+
+    alert('Thank you for your support')
+ }
+
+else if(questionThree === 'yes' || questionThree === 'y'){
+
+}
+
+alert('Soon to be veteran over here')
+
+let questionFour = prompt('Do you have a dream job');
+ // console.log(questionFour)
+ if(questionThree === 'no'  || questionThree === 'n'){
+
+    alert('You better go find it')
+ }
+
+else if(questionThree === 'yes' || questionThree === 'y'){
+
+    alert('I have a dream job to!')
 }
