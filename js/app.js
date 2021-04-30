bb0526188d14b2a031d76b194fe6e71ba32e2c53
albemedina <medinaa.atc@gmail.com>
2021-04-29 20:21:26 -0700

    updated

--- js/app.js
+++ js/app.js
@@ -1,68 +1,69 @@
-'use strict'
+'use strict';
 
 let username = prompt('Hey can I ask for your name ?');
 
-alert('Wanna talk for just a second '+ username +' ? Answer yes or no y/n');
+alert('Wanna talk for just a second ' + username + ' ? Answer yes or no y/n');
 
- let questionOne = prompt('Do you like Grey/s Anatomy? Type yes or no').toLowerCase();
-  // console.log(questionOne)
- if(questionOne === 'no'  || questionOne === 'n'){
-   
-   alert('me either I dont watch it')
+let questionOne = prompt('Do you like Grey/s Anatomy? Type yes or no').toLowerCase();
+// console.log(questionOne)
+if (questionOne === 'no' || questionOne === 'n') {
 
- }
-else if(questionOne === 'yes' || questionOne === 'y'){
-   alert('I still dont watch it')
+  alert('me either I dont watch it');
+
+}
+else if (questionOne === 'yes' || questionOne === 'y') {
+  alert('I still dont watch it');
 }
 
 
- let questionTwo = prompt('Do you live in washington state').toLowerCase();
- 
- // console.log(questionTwo)
- if(questionTwo === 'no'  || questionOne === 'n'){
-    
-    alert('If only I could leave');
- }
+let questionTwo = prompt('Do you live in washington state').toLowerCase();
+
+// console.log(questionTwo)
+if (questionTwo === 'no' || questionOne === 'n') {
+
+  alert('If only I could leave');
+}
 
 
-else if(questionTwo === 'yes' || questionTwo === 'y'){
-    
-    alert('Me too!')
+else if (questionTwo === 'yes' || questionTwo === 'y') {
+
+  alert('Me too!');
 }
 
 let questionThree = prompt('Have you ever been in the service').toLowerCase();
- // console.log(questionThree)
- if(questionThree === 'no'  || questionThree === 'n'){
+// console.log(questionThree)
+if (questionThree === 'no' || questionThree === 'n') {
 
-    alert('Thank you for your support')
- }
-
-else if(questionThree === 'yes' || questionThree === 'y'){
-
+  alert('Thank you for your support');
 }
 
-alert('Soon to be veteran over here')
+else if (questionThree === 'yes' || questionThree === 'y') {
+  alert('Soon to be veteran over here');
+}
+
 
 let questionFour = prompt('Do you have a dream job').toLowerCase();
- // console.log(questionFour)
- if(questionFour === 'no'  || questionFour === 'n'){
+// console.log(questionFour)
+if (questionFour === 'no' || questionFour === 'n') {
 
-    alert('You better go find it')
- }
+  alert('You better go find it');
+}
 
-else if(questionFour === 'yes' || questionFour === 'y'){
+else if (questionFour === 'yes' || questionFour === 'y') {
 
-    alert('I have a dream job to!')
+  alert('I have a dream job to!');
 }
 
 let questionFive = prompt('Did you enjoy this conversation').toLowerCase();
- // console.log(questionFive)
- if(questionFive === 'no'  || questionFive === 'n'){
+// console.log(questionFive)
+if (questionFive === 'no' || questionFive === 'n') {
 
-    alert('Oh well you win some you lose some')
- }
+  alert('Oh well you win some you lose some');
+}
 
- else if(questionFive === 'yes'  || questionFive ==='y'){
-    
-   alert('Have a great day with your redbull!')
- }
+else if (questionFive === 'yes' || questionFive === 'y') {
+
+  alert('Have a great day with your redbull!');
+}
+
+alert ('Thanks for the quick chat enjoy the red bull ' + username );
